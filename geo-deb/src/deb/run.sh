1c07e8aa6c8f35f4ac5747c85d6ee74f0a588042
Martin Ellis <martin.ellis@gov.scot>
2017-06-28 14:17:56 +0100

    MGS-3216 Add service unit file for systemd

--- geo-deb/src/deb/run.sh
+++ geo-deb/src/deb/run.sh
@@ -2,7 +2,11 @@
 export codepoint=/opt/os/codepoint/codepoint.jar
 exec /usr/bin/java \
   -Xmx64m \
-  -XX:GCTimeRatio=10 \
+  -XX:+UseParallelGC \
+  -XX:MinHeapFreeRatio=20 \
+  -XX:MaxHeapFreeRatio=40 \
+  -XX:GCTimeRatio=4 \
+  -XX:AdaptiveSizePolicyWeight=90 \
   -Dlogback.configurationFile=/opt/geosearch/logback.xml \
   -jar /opt/geosearch/*.jar \
   >> /var/log/geosearch/geosearch.log 2>&1
